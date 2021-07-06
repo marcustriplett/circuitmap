6d7b272e2fd0e74bee097a1984e76bc5220d7e94
Marcus <marcusanthonytriplett@gmail.com>
2021-07-06 15:20:54 -0400

    Added JITable masking functions.

--- adaprobe/optimise/cavi_offline_spike_and_slab_NOTS_jax.py
+++ adaprobe/optimise/cavi_offline_spike_and_slab_NOTS_jax.py
@@ -86,6 +86,7 @@ def update_lam(y, I, mu, beta, alpha, lam, shape, rate, phi, phi_cov, key, num_m
 
 	N = mu.shape[0]
 	for n in range(N):
+		mask = jnp.append(jnp.arange(n), jnp.arange(n + 1, N))
 		arg = -2 * y * mu[n] * alpha[n] + 2 * mu[n] * alpha[n] * jnp.sum(jnp.expand_dims(mu[mask] * alpha[mask], 1) * lam[mask], 0) \
 		+ (mu[n]**2 + beta[n]**2) * alpha[n]
 		mc_samps, key = sample_phi_independent_truncated_normals(key, phi[n], phi_cov[n]) # samples of phi for neuron n
