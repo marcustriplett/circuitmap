3cbc9c848e156a53ee2b12abe05d41747dd7fda3
Marcus Triplett <marcus.triplett@columbia.edu>
2021-07-29 12:06:14 -0400

    Restructuring code

--- adaprobe/optimise/cavi_sns.py
+++ adaprobe/optimise/cavi_sns.py
@@ -26,7 +26,7 @@ def cavi_sns(obs, I, mu_prior, beta_prior, alpha_prior, shape_prior, rate_prior,
 		lam_mask = jnp.ones(y.shape[0])
 
 	return _cavi_sns(y, I, mu_prior, beta_prior, alpha_prior, shape_prior, rate_prior, phi_prior, phi_cov_prior, 
-	iters, num_mc_samples, seed, lam_mask, N, K)
+	iters, num_mc_samples, seed, lam_mask)
 
 @jax.partial(jit, static_argnums=(9, 10, 11, 12))
 def _cavi_sns(y, I, mu_prior, beta_prior, alpha_prior, shape_prior, rate_prior, phi_prior, phi_cov_prior, 
