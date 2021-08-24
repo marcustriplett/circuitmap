e3f541caab247ea1fcb724beb5a2267d3c589654
Marcus Triplett <marcus.triplett@columbia.edu>
2021-08-24 11:16:46 -0400

    Testing multiplicative noise

--- adaprobe/optimise/__init__.py
+++ adaprobe/optimise/__init__.py
@@ -2,3 +2,4 @@ from .cavi_sns import cavi_sns
 from .mbcs import mbcs
 from .cosamp import cosamp
 from .mbcs_sparse_outliers import mbcs_sparse_outliers
+from .mbcs_multiplicative_noise import mbcs_multiplicative_noise
