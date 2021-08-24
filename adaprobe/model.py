ea0e3897d09abe66217cc72530f1ad7c462a84b0
Marcus Triplett <marcus.triplett@columbia.edu>
2021-08-24 11:37:34 -0400

    Testing multiplicative noise

--- adaprobe/model.py
+++ adaprobe/model.py
@@ -23,6 +23,9 @@ class Model:
 		if model_type == 'variational_sns':
 			self.priors.setdefault('alpha', 1/2 * _ones)
 
+		elif model_type == 'mbcs_multiplicative_noise':
+			self.priors.setdefault('rho_prior', 1e-1 * _ones)
+
 		self.priors.setdefault('shape', 1.)
 		self.priors.setdefault('rate', 1.)
 		self.priors.setdefault('mu', np.zeros(self.n_presynaptic))
@@ -60,6 +63,8 @@ class Model:
 			self._fit_mbcs(obs, stimuli, fit_options)
 		elif method == 'mbcs_sparse_outliers':
 			self._fit_mbcs_sparse_outliers(obs, stimuli, fit_options)
+		elif method =='mbcs_multiplicative_noise':
+			self._fit_mbcs_multiplicative_noise(obs, stimuli, fit_options)
 		elif method == 'cavi_sns':
 			self._fit_cavi_sns(obs, stimuli, fit_options)
 
@@ -239,3 +244,62 @@ class Model:
 
 		return
 
+	def _fit_mbcs_multiplicative_noise(obs, stimuli, fit_options):
+		"""
+		"""
+		t_start = time.time()
+		result = optimise.mbcs_sparse_outliers(
+			obs, stimuli, self.state['mu'], self.state['beta'], self.state['shape'], self.state['rate'], 
+			self.state['phi'], self.state['phi_cov'], self.state['rho_prior'], **fit_options 
+		)
+
+		t_end = time.time()
+
+		mu, beta, lam, shape, rate, phi, phi_cov, xi, rho, mu_hist, beta_hist, lam_hist, shape_hist, rate_hist, \
+		phi_hist, phi_cov_hist = result
+
+		# move from GPU back to CPU
+		## param vectors
+		mu 			= np.array(mu)
+		beta 		= np.array(beta)
+		lam 		= np.array(lam)
+		shape 		= np.array(shape)
+		rate 		= np.array(rate)
+		phi 		= np.array(phi)
+		phi_cov 	= np.array(phi_cov)
+		xi 			= np.array(xi)
+		rho 		= np.array(rho)
+
+		## history vectors
+		mu_hist 		= np.array(mu_hist)
+		beta_hist 		= np.array(beta_hist)
+		lam_hist 		= np.array(lam_hist)
+		shape_hist 		= np.array(shape_hist)
+		rate_hist 		= np.array(rate_hist)
+		phi_hist 		= np.array(phi_hist)
+		phi_cov_hist 	= np.array(phi_cov_hist)
+
+		self.state['mu'] 		= mu
+		self.state['beta'] 		= beta
+		self.state['shape'] 	= shape
+		self.state['rate'] 		= rate
+		self.state['phi'] 		= phi
+		self.state['phi_cov'] 	= phi_cov
+		self.state['lam'] 		= lam.T
+		self.state['xi'] 		= xi
+		self.state['rho']		= rho
+		self.trial_count 		= lam.shape[1]
+		self.time 				= t_end - t_start
+
+		# Set up history dict
+		self.history = {
+			'mu': mu_hist,
+			'beta': beta_hist,
+			'lam': lam_hist,
+			'shape': shape_hist,
+			'rate': rate_hist,
+			'phi': phi_hist,
+			'phi_cov': phi_cov_hist
+		}
+
+		return
