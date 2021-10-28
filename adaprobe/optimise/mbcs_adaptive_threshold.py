e95e5b262454668e0c350ab8e0b906da8b481865
Marcus Triplett <marcus.triplett@columbia.edu>
2021-10-28 10:54:51 -0400

    Added adaptive excitability thresholding

--- adaprobe/optimise/mbcs_adaptive_threshold.py
+++ adaprobe/optimise/mbcs_adaptive_threshold.py
@@ -78,6 +78,7 @@ def mbcs_adaptive_threshold(obs, I, mu_prior, beta_prior, shape_prior, rate_prio
 
 	# Iterate CAVI updates
 	for it in range(iters):
+		print('iter %i/%i'%(it+1, iters), end='\r')
 		beta = update_beta(lam, shape, rate, beta_prior)
 		mu = update_mu_constr_l1(y, mu, lam, shape, rate, penalty=penalty, scale_factor=scale_factor, 
 			max_penalty_iters=max_penalty_iters, max_lasso_iters=max_lasso_iters, warm_start_lasso=warm_start_lasso, 
