b05d973e361efb88370975ced156b1c0172c8ae7
Marcus Triplett <marcus.triplett@columbia.edu>
2021-11-02 10:51:25 -0400

    Combined sparse outlier inference with monotonicity constraint on phi_0

--- adaprobe/model.py
+++ adaprobe/model.py
@@ -300,8 +300,8 @@ class Model:
 
 		t_end = time.time()
 
-		mu, beta, lam, shape, rate, phi, phi_cov, mu_hist, beta_hist, lam_hist, shape_hist, rate_hist, \
-		phi_hist, phi_cov_hist = result
+		mu, beta, lam, shape, rate, phi, phi_cov, z, mu_hist, beta_hist, lam_hist, shape_hist, rate_hist, \
+		phi_hist, phi_cov_hist, z_hist = result
 
 		# move from GPU back to CPU
 		## param vectors
@@ -312,6 +312,7 @@ class Model:
 		rate 		= np.array(rate)
 		phi 		= np.array(phi)
 		phi_cov 	= np.array(phi_cov)
+		z 			= np.array(z)
 
 		## history vectors
 		mu_hist 		= np.array(mu_hist)
@@ -321,6 +322,7 @@ class Model:
 		rate_hist 		= np.array(rate_hist)
 		phi_hist 		= np.array(phi_hist)
 		phi_cov_hist 	= np.array(phi_cov_hist)
+		z_hist 			= np.array(z_hist)
 
 		self.state['mu'] 		= mu
 		self.state['beta'] 		= beta
@@ -329,6 +331,7 @@ class Model:
 		self.state['phi'] 		= phi
 		self.state['phi_cov'] 	= phi_cov
 		self.state['lam'] 		= lam
+		self.state['z'] 		= z
 		self.trial_count 		= lam.shape[1]
 		self.time 				= t_end - t_start
 
@@ -340,5 +343,6 @@ class Model:
 			'shape': shape_hist,
 			'rate': rate_hist,
 			'phi': phi_hist,
-			'phi_cov': phi_cov_hist
+			'phi_cov': phi_cov_hist,
+			'z_hist': z_hist
 		}
