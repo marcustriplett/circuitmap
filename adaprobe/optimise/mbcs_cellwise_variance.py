4db73a5ec9dc93952c3802471e0e903a358ed8cb
Marcus Triplett <marcus.triplett@columbia.edu>
2021-12-03 18:23:18 -0500

    Testing cellwise variance penalty

--- adaprobe/optimise/mbcs_cellwise_variance.py
+++ adaprobe/optimise/mbcs_cellwise_variance.py
@@ -128,9 +128,9 @@ def adaptive_excitability_threshold(mu, lam, I, phi, phi_thresh, minimum_spike_c
 		slopes[i] = linregress(powers, inferred_spk_probs[i]).slope
 		# slopes[i] = lr.fit(powers, inferred_spk_probs[i].reshape(-1, 1) - spont_rate).coef_[0, 0]
 
-	disc_cells = connected_cells[slopes < 0]
-	mu = index_update(mu, disc_cells, 0.)
-	lam = index_update(lam, disc_cells, 0.)
+	# disc_cells = connected_cells[slopes < 0]
+	# mu = index_update(mu, disc_cells, 0.)
+	# lam = index_update(lam, disc_cells, 0.)
 
 	# Filter connection vector via opsin expression threshold
 	phi_locs = np.where(phi[:, 0] < phi_thresh)[0]
