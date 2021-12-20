b61a0f76bbb338bebe8366cd14af3ea351f0a60a
Marcus Triplett <marcus.triplett@columbia.edu>
2021-12-20 15:55:21 -0500

    Testing spike-weighted noise variance

--- adaprobe/optimise/__init__.py
+++ adaprobe/optimise/__init__.py
@@ -5,3 +5,4 @@ from .mbcs_sparse_outliers import mbcs_sparse_outliers
 from .mbcs_multiplicative_noise import mbcs_multiplicative_noise
 from .mbcs_adaptive_threshold import mbcs_adaptive_threshold
 from .mbcs_cellwise_variance import mbcs_cellwise_variance
+from .mbcs_spike_weighted_var import mbcs_spike_weighted_var
