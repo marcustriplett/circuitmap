046356f4bfc90eb358bb19ff97b092e7745dc2a5
Marcus Triplett <marcus.triplett@columbia.edu>
2021-12-21 12:42:40 -0500

    Fixed noise updates

--- adaprobe/optimise/mbcs_spike_weighted_var.py
+++ adaprobe/optimise/mbcs_spike_weighted_var.py
@@ -137,14 +137,14 @@ def adaptive_excitability_threshold(mu, lam, I, phi, phi_thresh, minimum_spike_c
 		slopes[i] = linregress(powers, inferred_spk_probs[i]).slope
 
 	# Enforce non-negative slope constraint
-	disc_cells = connected_cells[slopes < 0]
-	mu = index_update(mu, disc_cells, 0.)
-	lam = index_update(lam, disc_cells, 0.)
+	# disc_cells = connected_cells[slopes < 0]
+	# mu = index_update(mu, disc_cells, 0.)
+	# lam = index_update(lam, disc_cells, 0.)
 
-	# Filter connection vector via opsin expression threshold
-	phi_locs = np.where(phi[:, 0] < phi_thresh)[0]
-	mu = index_update(mu, phi_locs, 0.)
-	lam = index_update(lam, phi_locs, 0.)
+	# # Filter connection vector via opsin expression threshold
+	# phi_locs = np.where(phi[:, 0] < phi_thresh)[0]
+	# mu = index_update(mu, phi_locs, 0.)
+	# lam = index_update(lam, phi_locs, 0.)
 
 	# Filter connection vector via spike counts
 	spks = np.array([len(np.where(lam[n] >= 0.5)[0]) for n in range(mu.shape[0])])
