836c1d02fe083de19a4c444ace06def21d5f9930
Marcus Triplett <marcus.triplett@columbia.edu>
2021-12-22 15:17:20 -0500

    Incorporated ghost cell mechanic for spontaneous pscs

--- adaprobe/optimise/mbcs_spike_weighted_var.py
+++ adaprobe/optimise/mbcs_spike_weighted_var.py
@@ -102,7 +102,7 @@ def mbcs_spike_weighted_var(obs, I, mu_prior, beta_prior, shape_prior, rate_prio
 		update_order = np.random.choice(N, N, replace=False)
 		lam = update_lam_with_isotonic_receptive_field(y, I, mu, beta, lam, shape, rate, lam_mask, update_order, spike_prior, num_mc_samples, N, spont_rate)
 		receptive_field, spike_prior = update_isotonic_receptive_field(lam, I)
-		mu, lam = isotonic_filtering(mu, lam, I, receptive_field, minimum_spike_count=minimum_spike_count, minimum_maximal_spike_prob=minimum_maximal_spike_prob)
+		mu[:-1], lam[:-1] = isotonic_filtering(mu[:-1], lam[:-1], I, receptive_field, minimum_spike_count=minimum_spike_count, minimum_maximal_spike_prob=minimum_maximal_spike_prob)
 		shape, rate = update_noise(y, mu, beta, lam, noise_scale=noise_scale, num_mc_samples=num_mc_samples_noise_model)
 
 		# (phi, phi_cov), key = update_phi(lam, I, phi_prior, phi_cov_prior, key)
