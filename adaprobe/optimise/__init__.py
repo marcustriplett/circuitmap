88670c0c4f93293798e9fdf2cc3d927ba455e6e8
Marcus Triplett <marcus.triplett@columbia.edu>
2021-12-23 13:33:28 -0500

    Readded sigmoidal prior

--- adaprobe/optimise/__init__.py
+++ adaprobe/optimise/__init__.py
@@ -6,3 +6,4 @@ from .mbcs_multiplicative_noise import mbcs_multiplicative_noise
 from .mbcs_adaptive_threshold import mbcs_adaptive_threshold
 from .mbcs_cellwise_variance import mbcs_cellwise_variance
 from .mbcs_spike_weighted_var import mbcs_spike_weighted_var
+from .mbcs_spike_weighted_var_with_sigmoid_rf import mbcs_spike_weighted_var_with_sigmoid_rf
