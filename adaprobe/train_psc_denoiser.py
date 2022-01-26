1fc43e999692c9b57fba2b8b585b7d8aebfb06c1
Marcus Triplett <marcus.triplett@columbia.edu>
2022-01-26 16:52:49 -0500

    Adjusted PSC denoiser runfile

--- adaprobe/train_psc_denoiser.py
+++ adaprobe/train_psc_denoiser.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
 	denoiser = NeuralDenoiser()
 	denoiser.generate_training_data(trial_dur=900, size=size, gp_scale=0.045, delta_lower=160,
 								delta_upper=400, next_delta_lower=400, next_delta_upper=899,
-								prev_delta_upper=150, tau_diff_lower=2, tau_diff_upper=150,
-								tau_r_lower=10, tau_r_upper=80)
+								prev_delta_upper=150, tau_diff_lower=150, tau_diff_upper=340,
+								tau_r_lower=10, tau_r_upper=40, noise_std_lower=0.001,
+								noise_std_upper=0.02, gp_lengthscale=45)
 	denoiser.train(epochs=epochs)
