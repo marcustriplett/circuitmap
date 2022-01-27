12b2036613480c3447a9c01b815a1079db22e34f
Marcus Triplett <marcus.triplett@columbia.edu>
2022-01-26 22:58:54 -0500

    Added template sampling to psc denoiser training

--- adaprobe/train_psc_denoiser.py
+++ adaprobe/train_psc_denoiser.py
@@ -5,15 +5,17 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--size')
 	parser.add_argument('--epochs')
+	parser.add_argument('--templates')
 	args = parser.parse_args()
 
 	size = int(args.size)
 	epochs = int(args.epochs)
+	templates = np.load(args.templates)
 
 	denoiser = NeuralDenoiser()
 	denoiser.generate_training_data(trial_dur=900, size=size, gp_scale=0.045, delta_lower=160,
 								delta_upper=400, next_delta_lower=400, next_delta_upper=899,
 								prev_delta_upper=150, tau_diff_lower=150, tau_diff_upper=340,
 								tau_r_lower=10, tau_r_upper=40, noise_std_lower=0.001,
-								noise_std_upper=0.02, gp_lengthscale=45)
+								noise_std_upper=0.02, gp_lengthscale=45, templates=templates)
 	denoiser.train(epochs=epochs)
