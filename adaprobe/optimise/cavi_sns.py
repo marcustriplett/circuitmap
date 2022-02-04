96b5c94a44159bb4e28acde26764a5ad7866ac1c
Marcus Triplett <marcus.triplett@columbia.edu>
2022-02-03 20:38:16 -0500

    Deleted lam row zeroing-out for testing

--- adaprobe/optimise/cavi_sns.py
+++ adaprobe/optimise/cavi_sns.py
@@ -80,7 +80,7 @@ def _cavi_sns(y, I, mu_prior, beta_prior, alpha_prior, shape_prior, rate_prior, 
 				# Filter connection vector via opsin expression threshold
 				scope.phi_expand = scope.phi[:, 0][0] * jnp.ones((N, K)) # does NOT select first element, instead selects entire vector
 				scope.mu = jnp.where(scope.phi[:, 0] >= phi_thresh, scope.mu, 0.) * (it > phi_thresh_delay) + scope.mu * (it <= phi_thresh_delay)
-				scope.lam = jnp.where(scope.phi[:, 0] >= phi_thresh, scope.lam, 0.) * (it > phi_thresh_delay) + scope.lam * (it <= phi_thresh_delay)
+				scope.lam = jnp.where((scope.phi[:, 0] >= phi_thresh).T, scope.lam, 0.) * (it > phi_thresh_delay) + scope.lam * (it <= phi_thresh_delay)
 
 			for hindx, pa in enumerate([scope.mu, scope.beta, scope.alpha, scope.lam, scope.shape, scope.rate, scope.phi, scope.phi_cov]):
 				scope.hist_arrs[hindx] = index_update(scope.hist_arrs[hindx], it, pa)
