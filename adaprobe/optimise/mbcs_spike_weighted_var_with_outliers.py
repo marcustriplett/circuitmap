cc96df53e8ba57d5e6f7dd04e9c34f1b41f2abb6
Marcus Triplett <marcus.triplett@columbia.edu>
2022-02-13 14:08:00 -0500

    Testing Newton inference of spikes with iterative penalty shrinkage

--- adaprobe/optimise/mbcs_spike_weighted_var_with_outliers.py
+++ adaprobe/optimise/mbcs_spike_weighted_var_with_outliers.py
@@ -33,7 +33,7 @@ def mbcs_spike_weighted_var_with_outliers(y_psc, I, mu_prior, beta_prior, shape_
 	learn_noise=False, init_lam=None, learn_lam=True, delay_spont_estimation=1, minimum_spike_count=1, noise_scale=0.5, 
 	num_mc_samples_noise_model=10, minimum_maximal_spike_prob=0.2, orthogonal_outliers=True, outlier_penalty=5e1, 
 	init_spike_prior=0.75, outlier_tol=0.05, spont_rate=0, lam_mask_fraction=0.05, lam_iters=1, newton_penalty=1e1,
-	newton_iters=20):
+	newton_iters=20, newton_penalty_shrinkage_iters=5):
 	"""Offline-mode coordinate ascent variational inference for the adaprobe model.
 	"""
 
@@ -94,7 +94,7 @@ def mbcs_spike_weighted_var_with_outliers(y_psc, I, mu_prior, beta_prior, shape_
 		lam = jnp.where(lam < 1e-5, 1e-5, lam)
 		print('Updating lam')
 		lam = update_lam_backtracking_newton(y, lam, tar_matrix, mu, lam_mask, shape, rate, penalty=newton_penalty, scale_factor=scale_factor, 
-			max_penalty_iters=max_penalty_iters, barrier_iters=5, t=1e0, barrier_multiplier=1e1, max_backtrack_iters=20, backtrack_alpha=0.05,
+			max_penalty_iters=newton_penalty_shrinkage_iters, barrier_iters=5, t=1e0, barrier_multiplier=1e1, max_backtrack_iters=20, backtrack_alpha=0.05,
 			backtrack_beta=0.75, verbose=verbose, newton_iters=newton_iters)
 		print()
 
@@ -172,6 +172,9 @@ def update_lam_backtracking_newton(y, lam, tar_matrix, mu, lam_mask, shape, rate
 	N, K = lam.shape
 	constr = np.sqrt(np.sum(rate/shape))
 	err_prev = 0
+
+	if verbose:
+			print(' ====== Updating lam via constrained L1 solver with iterative penalty shrinking ======')
 
 	for it in range(max_penalty_iters):
 		if verbose:
