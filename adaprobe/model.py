893ac3212f63d5d15126c54472b808d1bd4773c9
Marcus Triplett <marcus.triplett@columbia.edu>
2022-02-14 14:50:28 -0500

    HALS relevance regression update for mu

--- adaprobe/model.py
+++ adaprobe/model.py
@@ -726,7 +726,7 @@ class Model:
 
 		t_end = time.time()
 
-		mu, beta, lam, shape, rate, z, rfs, mu_hist, beta_hist, lam_hist, shape_hist, rate_hist, z_hist, \
+		mu, beta, lam, shape, rate, z, rfs, relevance, mu_hist, beta_hist, lam_hist, shape_hist, rate_hist, z_hist, \
 			relevance_hist = result
 
 		# move from GPU back to CPU
