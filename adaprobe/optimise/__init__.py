3e4649b3429ba914867cdf8e7cd84bc9a341a12e
Marcus Triplett <marcus.triplett@columbia.edu>
2022-04-05 13:55:14 -0400

    Revamped caviar code, separated caviar from cavi_sns

--- adaprobe/optimise/__init__.py
+++ adaprobe/optimise/__init__.py
@@ -1,6 +1,7 @@
 from .cavi_sns import cavi_sns
 from .mbcs import mbcs
 from .cosamp import cosamp
+from .caviar import caviar
 from .mbcs_sparse_outliers import mbcs_sparse_outliers
 from .mbcs_multiplicative_noise import mbcs_multiplicative_noise
 from .mbcs_adaptive_threshold import mbcs_adaptive_threshold
