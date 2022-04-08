Fixed normalisation of spontaneous PSC kernels

adaprobe/simulation.py
@@ -296,7 +296,7 @@ get_true_evoked_resp_vmap = jit(vmap(get_true_evoked_resp, in_axes=(1, 1, None, 
 
 def simulate_continuous_experiment(N=100, expt_len=int(2e4), gamma_beta=1.5e1, min_latency=60, powers=[45, 55, 65],
 	mult_noise_log_var=0.05, response_length=900, noise_std=1e-2, tau_r_min=10, tau_r_max=40, tau_delta_min=250, tau_delta_max=300, sampling_freq=20000, 
-	stim_freq=10, weight_lower=2, weight_upper=10, seed=0, ar_coef=0.95, ar_std=1e-1, weights=None, frac_strongly_connected=0.2, strong_weight_lower=20, 
+	stim_freq=10, weight_lower=2, weight_upper=10, seed=0, ar_coef=0.95, ar_std=3e-4, weights=None, frac_strongly_connected=0.2, strong_weight_lower=20, 
 	strong_weight_upper=40, weak_exp_mean=4, min_weight=5, phi_0_lower=0.2, phi_0_upper=0.25, phi_1_lower=10, phi_1_upper=15, kernel=None, phi_0=None, phi_1=None,
 	H=10, nreps=1, connection_prob=0.1, spont_rate=10, kernel_window=3000, prior_context=100, ground_truth_eval_batch_size=1000):
 	'''Simulate continuous mapping experiment (to be sliced and reshaped post-hoc).
@@ -399,7 +399,7 @@ def simulate_continuous_experiment(N=100, expt_len=int(2e4), gamma_beta=1.5e1, m
 	psc_kernels = get_unnormalised_psc_kernel(tau_r, tau_d, kernel_window)
 	kernel_divisor = np.trapz(psc_kernels, axis=-1)
 
-	sponts = np.array(eval_sponts(trange, tau_r, tau_d, spont_times, np.random.uniform(min_weight, np.max(weights), [nspont]), kernel_divisor))
+	sponts = np.array(eval_sponts(trange, tau_r, tau_d, spont_times, np.random.uniform(0., np.max(weights), [nspont]), kernel_divisor))
 
 	# compute correlated noise
 	ar1_noise = np.zeros(expt_len)