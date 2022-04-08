Fixed normalisation of spontaneous PSC kernels

adaprobe/simulation.py
@@ -258,6 +258,12 @@ def _get_psc_kernel(tau_r, tau_d, kernel_window, eps=1e-5):
 	return ke/(jnp.trapz(ke) + eps)
 get_psc_kernel = jit(vmap(_get_psc_kernel, in_axes=(0, 0, None)), static_argnums=(2))
 
+def _get_unnormalised_psc_kernel(tau_r, tau_d, kernel_window, eps=1e-5):
+	krange = jnp.arange(kernel_window)
+	ke = jnp.exp(-krange/tau_d) - jnp.exp(-krange/tau_r) # normalised kernel
+	return ke
+get_unnormalised_psc_kernel = jit(vmap(_get_unnormalised_psc_kernel, in_axes=(0, 0, None)), static_argnums=(2))
+
 def _kernel_conv(trange, psc_kernel, delta, spike, mult_noise, weight):
 	''' Warning: assumes no spike occurs on very first bin due to jax workaround.
 	'''
@@ -390,7 +396,7 @@ def simulate_continuous_experiment(N=100, expt_len=int(2e4), gamma_beta=1.5e1, m
 	tau_r = np.random.uniform(tau_r_min, tau_r_max, nspont)
 	tau_delta = np.random.uniform(tau_delta_min, tau_delta_max, nspont)
 	tau_d = tau_r + tau_delta
-	psc_kernels = get_psc_kernel(tau_r, tau_d, kernel_window)
+	psc_kernels = get_unnormalised_psc_kernel(tau_r, tau_d, kernel_window)
 	kernel_divisor = np.trapz(psc_kernels, axis=-1)
 
 	sponts = np.array(eval_sponts(trange, tau_r, tau_d, spont_times, np.random.uniform(weight_lower, weight_upper, [nspont]), kernel_divisor))