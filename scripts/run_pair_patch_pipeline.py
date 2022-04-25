d9fb8a55549cf8d75789202d5bb22b12c21d8253
Marcus Triplett <marcus.triplett@columbia.edu>
2022-04-25 16:10:06 -0400

    Fixed missing variables due to psc_dem subset extraction

--- scripts/run_pair_patch_pipeline.py
+++ scripts/run_pair_patch_pipeline.py
@@ -8,6 +8,94 @@ from scipy.io import loadmat
 def lookup(coords, arr):
 	return np.intersect1d(*[np.where(arr[:, i] == coords[i])[0] for i in range(2)])[0]
 
+def plot_spike_inference_comparison(den_pscs, stim_matrices, models, spks=None, titles=None, save=None, ymax=1.1, n_plots=15, max_trials_to_show=30, 
+										 col_widths=None, row_height=0.6, order=None, trial_len=900, lp_cell=None):
+	if col_widths is None:
+		col_widths = 7.5 * np.ones(len(models))
+		
+	N = stim_matrices[0].shape[0]
+	Is = [np.array([np.unique(stim[:, k])[1] for k in range(stim.shape[1])])
+		 for stim in stim_matrices]
+	ncols = len(models)
+	
+	fig = plt.figure(figsize=(np.sum(col_widths), row_height * n_plots * 1.5))
+	gs = fig.add_gridspec(ncols=ncols, nrows=n_plots, hspace=0.5, wspace=0.05, width_ratios=col_widths/col_widths[0])
+	
+	normalisation_factor = np.max(np.abs(np.vstack(den_pscs)))
+	mu_norm = np.max(np.abs([model.state['mu'] for model in models]))
+	ymin = -0.05 * ymax
+	
+	trace_linewidth = 0.65
+	
+	if order is None:
+		order = np.argsort(models[0].state['mu'])[::-1]
+	
+	for col in range(ncols):
+		for m in range(n_plots):
+			n = order[m]
+
+			# spike predictions
+			ax = fig.add_subplot(gs[m, col])
+			if m == 0 and titles is not None:
+				plt.title(titles[col], fontsize=fontsize, y=1.5)
+
+			powers = np.unique(Is[col])
+			trials_per_power = max_trials_to_show // len(powers)
+			stim_locs = np.array([])
+			for pwr in powers:
+				stim_locs = np.concatenate([stim_locs, np.where(stim_matrices[col][n] == pwr)[0][:trials_per_power]])
+
+			stim_locs = stim_locs.astype(int)
+			this_y_psc = den_pscs[col][stim_locs].flatten()/normalisation_factor
+			n_repeats = np.min([len(stim_locs), max_trials_to_show])
+			trial_breaks = np.arange(0, trial_len * n_repeats + 1, trial_len)
+
+			plt.xlim([0, trial_len*n_repeats])
+			facecol = 'firebrick' if n != lp_cell else 'C0'
+			model = models[col]
+			lam = model.state['lam']
+			K = lam.shape[1]
+			mu = model.state['mu'].copy()
+			trace_col = 'k' if mu[n] != 0 else 'gray'
+			
+			if 'z' in model.state.keys():
+				z = model.state['z']
+			else:
+				z = np.zeros(K)
+				
+			for tb in range(len(trial_breaks) - 1):
+				if tb > 0:
+					plt.plot([trial_breaks[tb], trial_breaks[tb]], [ymin, ymax], '--', color=trace_col)
+					
+				ax.fill_between(np.arange(trial_len * tb, trial_len * (tb + 1)), ymin * np.ones(trial_len), ymax * np.ones(trial_len), facecolor=facecol, 
+								 edgecolor='None', alpha=lam[n, stim_locs][tb] * 0.5, zorder=-5)
+
+				# Plot power changes
+				if (m == 0) and (Is[col][stim_locs][tb] != Is[col][stim_locs][tb-1]):
+					plt.text(trial_breaks[tb], 1.1 * ymax, '%i mW'%Is[col][stim_locs][tb], fontsize=fontsize-2)
+				   
+				if z[stim_locs][tb] != 0:
+					plt.plot(trial_len * (tb + 0.5), 0.7 * ymax, marker='*', markerfacecolor='b', markeredgecolor='None', markersize=6)
+
+			plt.plot(this_y_psc, color=trace_col, linewidth=trace_linewidth)
+
+			for loc in ['top', 'right', 'left', 'bottom']:
+				plt.gca().spines[loc].set_visible(False)
+			plt.xticks([])
+			plt.yticks([])
+			plt.ylim([ymin, ymax])
+			
+			if col == 0:
+				label_col = 'k'
+				plt.ylabel(m+1, fontsize=fontsize-1, rotation=0, labelpad=15, va='center', color=label_col)
+
+				fig.supylabel('Neuron', fontsize=fontsize, x=0.09) #x=0.0825)
+			ax.set_rasterization_zorder(-2)
+	
+	if save is not None:
+		plt.savefig(save, format='png', bbox_inches='tight', dpi=300, facecolor='white')
+	plt.show()
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--data')
@@ -25,7 +113,7 @@ if __name__ == '__main__':
 
 	demix = NeuralDemixer(path=args.demixer, device='cpu')
 
-	N = targets.shape[0]
+	N, K = stim_matrix.shape[0]
 	msrmp = float(args.msrmp)
 
 	model_single, model_multi = cm.Model(N), cm.Model(N)
@@ -36,11 +124,13 @@ if __name__ == '__main__':
 	stim_single = stim_matrix[:, single_tar_locs]
 	stim_multi = stim_matrix[:, multi_tar_locs]
 
-	psc_single, psc_multi = [demix(arr) for arr in [psc[single_tar_locs], psc[multi_tar_locs]]]
+	psc_dem = demix(psc)
+	psc_single, psc_multi = psc[single_tar_locs], psc[multi_tar_locs]
+	psc_dem_single, psc_dem_multi = psc_dem[single_tar_locs], psc_dem[multi_tar_locs]
 
-	print('Fitting models...')
-	for model, psc, stim in zip([model_single, model_multi], [psc_single, psc_multi], [stim_single, stim_multi]):
-		model.fit(psc, stim, method='caviar', fit_options={'minimax_spk_prob': msrmp})
+	print('Fitting models...[TESTING]')
+	# for model, psc, stim in zip([model_single, model_multi], [psc_dem_single, psc_dem_multi], [stim_single, stim_multi]):
+	# 	model.fit(psc, stim, method='caviar', fit_options={'minimax_spk_prob': msrmp})
 	print('Complete.')
 
 	#% PLOTTING
@@ -193,6 +283,10 @@ if __name__ == '__main__':
 	out = args.out
 	if out[-1] != '/': out += '/'
 	fn = args.data.split('/')[-1][:-4] # extract filename and strip ext
-	fig.savefig('%s_%s_msrmp%s.pdf'%(args.out, fn, args.msrmp), format='pdf', bbox_inches='tight', facecolor='white', dpi=400)
-	fig.savefig('%s_%s_msrmp%s.png'%(args.out, fn, args.msrmp), format='png', bbox_inches='tight', facecolor='white', dpi=400)
+	fig.savefig('%s_%s_msrmp%s_summary.pdf'%(args.out, fn, args.msrmp), format='pdf', bbox_inches='tight', facecolor='white', dpi=400)
+	fig.savefig('%s_%s_msrmp%s_summary.png'%(args.out, fn, args.msrmp), format='png', bbox_inches='tight', facecolor='white', dpi=400)
+
+	psc_dems = [psc_dem_single, psc_dem_multi]
+	stim_matrices = [stim_single, stim_multi]
+	models = [model_single, model_multi]
 
