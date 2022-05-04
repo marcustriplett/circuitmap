ea1c786f688154e5722401533786fb441921300d
Marcus Triplett <marcus.triplett@columbia.edu>
2022-05-04 14:07:25 -0400

    Added checkerboard comparison to viz code

--- circuitmap/viz.py
+++ circuitmap/viz.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_checkerboard(psc, stim_matrix, model, true_spikes=None, true_weights=None, spike_thresh=0.01, save=None, ymax=None, n_plots=15, max_trials_to_show=30, 
-	col_width=7.5, row_height=0.6, order=None, sdevs=None, fig_width=None, overlay_spikes=True, annotate_spikes=False, wspace=0.05, 
-	hspace=0.5, ylabelpad=0.05, facecol=None, edgecol=None, trial_len=900, save_fmt='png', fontsize=14):
+	col_width=7.5, row_height=0.6, order=None, sdevs=None, fig_width=None, overlay_spikes=False, annotate_spikes=False, wspace=0.05, 
+	hspace=0.5, ylabelpad=0.05, facecol=None, edgecol=None, trial_len=900, save_fmt='png', fontsize=14, append_last_row=False):
 	''' plot_checkerboard
 	'''
 
@@ -98,7 +98,7 @@ def plot_checkerboard(psc, stim_matrix, model, true_spikes=None, true_weights=No
 		
 		# Overlay true spikes
 		if overlay_spikes:
-			if true_weights[n] != 0:
+			if mu[n] != 0:
 				spk_times = np.array([trial_breaks[tb] for tb in range(len(trial_breaks)-1) if true_spikes[n][stim_locs][tb] == 1])
 				plt.scatter(spk_times + trial_len//2, 0.75 * ymax * np.ones_like(spk_times), 20, marker='v', edgecolor='k', facecolor='None', linewidth=0.5)
 
@@ -108,7 +108,7 @@ def plot_checkerboard(psc, stim_matrix, model, true_spikes=None, true_weights=No
 		plt.yticks([])
 		plt.ylim([ymin, ymax])
 		if m % 4 == 0:
-			label_col = 'k' if true_weights[n] != 0 else 'gray'
+			label_col = 'k' if mu[n] != 0 else 'gray'
 			plt.ylabel(m+1, fontsize=fontsize-1, rotation=0, labelpad=15, va='center', color=label_col)
 		
 		ax.set_rasterization_zorder(-2)
@@ -116,34 +116,123 @@ def plot_checkerboard(psc, stim_matrix, model, true_spikes=None, true_weights=No
 	fig.add_subplot(gs[n_plots])
 	plt.axis('off')
 	
-	ax = fig.add_subplot(gs[n_plots + 1])
-	n = N-1
-	stim_locs = np.array([])  
-	for pwr in powers:
-		stim_locs = np.concatenate([stim_locs, np.where(stim_matrix[n] == pwr)[0][:trials_per_power]])
 
-	stim_locs = stim_locs.astype(int)
-	this_y_psc = psc[stim_locs].flatten()/normalisation_factor
-	n_repeats = np.min([len(stim_locs), max_trials_to_show])
-	trial_breaks = np.arange(0, trial_len * n_repeats + 1, trial_len)
+	if append_last_row:
+		ax = fig.add_subplot(gs[n_plots + 1])
+		n = N-1
+		stim_locs = np.array([])  
+		for pwr in powers:
+			stim_locs = np.concatenate([stim_locs, np.where(stim_matrix[n] == pwr)[0][:trials_per_power]])
 
-	plt.xlim([0, trial_len*n_repeats])
-	trace_col = 'gray'
-	for tb in range(len(trial_breaks) - 1):
-		if tb > 0:
-			plt.plot([trial_breaks[tb], trial_breaks[tb]], [ymin, ymax], '--', color=trace_col)
+		stim_locs = stim_locs.astype(int)
+		this_y_psc = psc[stim_locs].flatten()/normalisation_factor
+		n_repeats = np.min([len(stim_locs), max_trials_to_show])
+		trial_breaks = np.arange(0, trial_len * n_repeats + 1, trial_len)
 
-	plt.plot(this_y_psc, color=trace_col, linewidth=trace_linewidth)
-	plt.xticks([])
-	plt.yticks([])
-	plt.ylim([ymin, ymax])
-	for loc in ['top', 'right', 'left', 'bottom']:
-		plt.gca().spines[loc].set_visible(False)
-	plt.xlabel('Trials', fontsize=fontsize)
-	plt.ylabel(N, fontsize=fontsize-1, rotation=0, labelpad=15, va='center', color='gray')
+		plt.xlim([0, trial_len*n_repeats])
+		trace_col = 'gray'
+		for tb in range(len(trial_breaks) - 1):
+			if tb > 0:
+				plt.plot([trial_breaks[tb], trial_breaks[tb]], [ymin, ymax], '--', color=trace_col)
+
+		plt.plot(this_y_psc, color=trace_col, linewidth=trace_linewidth)
+		plt.xticks([])
+		plt.yticks([])
+		plt.ylim([ymin, ymax])
+		for loc in ['top', 'right', 'left', 'bottom']:
+			plt.gca().spines[loc].set_visible(False)
+		plt.xlabel('Trials', fontsize=fontsize)
+		plt.ylabel(N, fontsize=fontsize-1, rotation=0, labelpad=15, va='center', color='gray')
 	
 	fig.supylabel('Neuron', fontsize=fontsize, x=ylabelpad)
 	if save is not None:
 		plt.savefig(save, format=save_fmt, bbox_inches='tight', dpi=300, backend='pgf')
 
+	plt.show()
+
+def plot_spike_inference_comparison(den_pscs, stim_matrices, models, spks=None, titles=None, save=None, ymax=1.1, n_plots=15, max_trials_to_show=30, 
+	col_widths=None, row_height=0.6, order=None, trial_len=900, lp_cell=None):
+	if col_widths is None:
+		col_widths = 7.5 * np.ones(len(models))
+		
+	N = stim_matrices[0].shape[0]
+	Is = [np.array([np.unique(stim[:, k])[1] for k in range(stim.shape[1])])
+		 for stim in stim_matrices]
+	ncols = len(models)
+	
+	fig = plt.figure(figsize=(np.sum(col_widths), row_height * n_plots * 1.5))
+	gs = fig.add_gridspec(ncols=ncols, nrows=n_plots, hspace=0.5, wspace=0.05, width_ratios=col_widths/col_widths[0])
+	
+	normalisation_factor = np.max(np.abs(np.vstack(den_pscs)))
+	mu_norm = np.max(np.abs([model.state['mu'] for model in models]))
+	ymin = -0.05 * ymax
+	
+	trace_linewidth = 0.65
+	
+	if order is None:
+		order = get_cell_order([model.state['mu'] for model in models])
+	
+	for col in range(ncols):
+		for m in range(n_plots):
+			n = order[m]
+
+			# spike predictions
+			ax = fig.add_subplot(gs[m, col])
+			if m == 0 and titles is not None:
+				plt.title(titles[col], fontsize=fontsize, y=1.5)
+
+			powers = np.unique(Is[col])
+			trials_per_power = max_trials_to_show // len(powers)
+			stim_locs = np.array([])
+			for pwr in powers:
+				stim_locs = np.concatenate([stim_locs, np.where(stim_matrices[col][n] == pwr)[0][:trials_per_power]])
+
+			stim_locs = stim_locs.astype(int)
+			this_y_psc = den_pscs[col][stim_locs].flatten()/normalisation_factor
+			n_repeats = np.min([len(stim_locs), max_trials_to_show])
+			trial_breaks = np.arange(0, trial_len * n_repeats + 1, trial_len)
+
+			plt.xlim([0, trial_len*n_repeats])
+			facecol = 'firebrick' if n != lp_cell else 'C0'
+			model = models[col]
+			lam = model.state['lam']
+			K = lam.shape[1]
+			mu = model.state['mu'].copy()
+			trace_col = 'k' if mu[n] != 0 else 'gray'
+			
+			if 'z' in model.state.keys():
+				z = model.state['z']
+			else:
+				z = np.zeros(K)
+				
+			for tb in range(len(trial_breaks) - 1):
+				if tb > 0:
+					plt.plot([trial_breaks[tb], trial_breaks[tb]], [ymin, ymax], '--', color=trace_col)
+					
+				ax.fill_between(np.arange(trial_len * tb, trial_len * (tb + 1)), ymin * np.ones(trial_len), ymax * np.ones(trial_len), facecolor=facecol, 
+								 edgecolor='None', alpha=lam[n, stim_locs][tb] * 0.5, zorder=-5)
+
+				# Plot power changes
+				if (m == 0) and (Is[col][stim_locs][tb] != Is[col][stim_locs][tb-1]):
+					plt.text(trial_breaks[tb], 1.1 * ymax, '%i mW'%Is[col][stim_locs][tb], fontsize=fontsize-2)
+				   
+				if z[stim_locs][tb] != 0:
+					plt.plot(trial_len * (tb + 0.5), 0.7 * ymax, marker='*', markerfacecolor='b', markeredgecolor='None', markersize=6)
+
+			plt.plot(this_y_psc, color=trace_col, linewidth=trace_linewidth)
+
+			for loc in ['top', 'right', 'left', 'bottom']:
+				plt.gca().spines[loc].set_visible(False)
+			plt.xticks([])
+			plt.yticks([])
+			plt.ylim([ymin, ymax])
+			
+			if col == 0:
+				label_col = 'k'
+				plt.ylabel(m+1, fontsize=fontsize-1, rotation=0, labelpad=15, va='center', color=label_col)
+
+			ax.set_rasterization_zorder(-2)
+	
+	if save is not None:
+		plt.savefig(save, format='png', bbox_inches='tight', dpi=300, facecolor='white')
 	plt.show()
