54e14452bde986c0e78ad508261690159c2dea8b
Marcus Triplett <marcus.triplett@columbia.edu>
2022-05-17 15:12:06 -0400

    Added neurocaas bash and py runfiles

---
+++ scripts/run_circuitmap_main.py
@@ -0,0 +1,59 @@
+import argparse
+import numpy as np
+from circuitmap.neural_waveform_demixing import NeuralDemixer
+import circuitmap as cm
+from scipy.io import savemat, loadmat
+import yaml
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--data')
+	parser.add_argument('--config')
+	parser.add_argument('--out')
+
+	args = parser.parse_args()
+
+	# parse inputs
+	ext = args.file[-4:]
+	if ext == '.mat':
+		load_func = loadmat
+	elif ext == '.npy' or ext == '.npz':
+		load_func = np.load
+	else:
+		raise Exception
+
+	f = load_func(args.path + args.file)
+	psc = f['psc']
+	stim_matrix = f['stimulus_matrix']
+
+	# read demixer and msrmp from config file
+	config = yaml.safe_load(open(args.config))
+	save_fmt = config['save_fmt']
+
+	assert save_fmt in ['mat', 'npy']
+
+	# place demixer on cpu to avoid memory clash between pytorch and JAX
+	demix = NeuralDemixer(path=config['demixer'], device='cpu')
+	psc_dem = demix(psc)
+
+	# setup circuitmap model
+	msrmp = float(config['msrmp'])
+	N = stim_matrix.shape[0]
+	model = cm.Model(N)
+
+	# fit model
+	cm.fit(psc_dem, stim_matrix, method='caviar', fit_options={'msrmp': msrmp})
+
+	# save results
+	out = args.out
+	if out[-1] != '/':
+		out += '/'
+
+	save_name = args.out + args.file[:-4] + '_cmap.' + save_fmt
+
+	if save_fmt == 'mat':
+		savemat(save_name, {'weights': cm.state['mu'], 'weight_uncertainty': cm.state['beta'],
+			'spikes': cm.state['lam']})
+	else:
+		np.savez(save_name + save_fmt, weights=cm.state['mu'], weight_uncertainty=cm.state['beta'], 
+			spikes=cm.state['lam'])
---
+++ scripts/run_circuitmap_main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+userhome="/home/ubuntu"
+datastore="circuitmap/data"
+# outstore="ncapdata/localout"
+
+echo "---- DOWNLOADING DATA ----"
+conda activate jax
+neurocaas-contrib workflow get-data -f -o $userhome/$datastore/
+neurocaas-contrib workflow get-config -f -o $userhome/$datastore/
+
+echo "---- PARSING PATHS ----"
+datapath=$(neurocaas-contrib workflow get-datapath)
+configpath=$(neurocaas-contrib workflow get-configpath)
+resultpath=$(neurocaas-contrib workflow get-resultpath-tmp)
+
+cd "$userhome/circuitmap"
+
+echo "---- LAUNCHING CIRCUITMAP ----"
+python "scripts/run_circuitmap_main.py" --data $datapath --config $configpath --out $resultpath
+
+# echo "---- WRITING RESULTS ----"
+# neurocaas-contrib workflow put-result -r $resultpath/
