732685352755f6f9b80ad9c98f7ecccbea7de07c
Marcus Triplett <marcus.triplett@columbia.edu>
2022-05-24 10:56:19 -0400

    Updated neurocaas analysis bash script

--- scripts/run_circuitmap_main.sh
+++ scripts/run_circuitmap_main.sh
@@ -6,7 +6,6 @@ source activate neurocaas
 userhome="/home/ubuntu/"
 
 echo "---- DOWNLOADING DATA ----"
-
 neurocaas-contrib workflow get-data
 neurocaas-contrib workflow get-config
 
@@ -14,12 +13,14 @@ echo "---- PARSING PATHS ----"
 datapath=$(neurocaas-contrib workflow get-datapath)
 configpath=$(neurocaas-contrib workflow get-configpath)
 resultpath=$(neurocaas-contrib workflow get-resultpath-tmp)
+source deactivate
 
 echo "---- LAUNCHING CIRCUITMAP ----"
 cd $userhome/circuitmap
 source activate pytorch_p38
 python ./scripts/run_circuitmap_main.py --data $datapath --config $configpath --out $resultpath
-source activate neurocaas
+source deactivate
 
 echo "---- UPLOADING RESULTS ----"
+source activate neurocaas
 neurocaas-contrib workflow put-result -r $resultpath/*
