03a155c71a517dbd363d1bda91dcdb3c41fade25
Marcus Triplett <marcus.triplett@columbia.edu>
2022-05-24 11:28:24 -0400

    Renamed compressed result folders

--- scripts/run_circuitmap_main.sh
+++ scripts/run_circuitmap_main.sh
@@ -15,15 +15,15 @@ datapath=$(neurocaas-contrib workflow get-datapath)
 configpath=$(neurocaas-contrib workflow get-configpath)
 resultpath=$(neurocaas-contrib workflow get-resultpath-tmp)
 dataname=$(neurocaas-contrib workflow get-dataname)
-dataname=${dataname%.*}
+savname="${dataname%.*}_cm"
 source deactivate
 
 echo "---- LAUNCHING CIRCUITMAP ----"
 source activate pytorch_p38
 python $userhome/circuitmap/scripts/run_circuitmap_main.py --data $datapath --config $configpath --out $resultpath
-zip -r dataname.zip $resultpath/*
+zip -r $savname.zip $resultpath/*
 source deactivate
 
 echo "---- UPLOADING RESULTS ----"
 source activate neurocaas
-neurocaas-contrib workflow put-result -r dataname.zip
+neurocaas-contrib workflow put-result -r $savname.zip
