0a6ec72064d026a89240d9846acb518d9bcdeca0
Marcus Triplett <marcus.triplett@columbia.edu>
2023-07-17 14:43:10 -0400

    Added script for training new PSC demixers

---
+++ scripts/train_psc_demixer.py
@@ -0,0 +1,52 @@
+import argparse
+import numpy as np
+from circuitmap.neural_waveform_demixing import NeuralDemixer
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--size')
+	parser.add_argument('--epochs')
+	parser.add_argument('--templates')
+	parser.add_argument('--pretrained')
+	args = parser.parse_args()
+
+	size = int(args.size)
+	epochs = int(args.epochs)
+
+	# Optionally load negative training templates
+	if args.templates is None:
+		templates = None
+	else:
+		templates = np.load(args.templates)
+
+	# Optionally load pretrained model
+	if args.pretrained is None:
+		demixer = NeuralDemixer()
+	else:
+		demixer = NeuralDemixer(path=args.pretrained)
+
+	# Params for chrome2f + interneuron -> pyramidal currents
+	# tau_r_lower = 10
+	# tau_r_upper = 40
+	# tau_diff_lower = 150
+	# tau_diff_upper = 340
+
+	# Params for chrome1 + pyramidal -> pyramidal currents
+	# tau_r_lower = 10
+	# tau_r_upper = 40
+	# tau_diff_lower = 60
+	# tau_diff_upper = 120
+
+	# Params for pyramidal -> PV synaptic currents
+	tau_r_lower = 3
+	tau_r_upper = 20
+	tau_diff_lower = 20 - tau_r_lower
+	tau_diff_upper = 110 - tau_r_upper
+
+	demixer.generate_training_data(trial_dur=900, size=size, gp_scale=0.045, delta_lower=160,
+								delta_upper=400, next_delta_lower=400, next_delta_upper=899,
+								prev_delta_upper=150, tau_diff_lower=tau_diff_lower, 
+								tau_diff_upper=tau_diff_upper, tau_r_lower=tau_r_lower, 
+								tau_r_upper=tau_r_upper, noise_std_lower=0.001,
+								noise_std_upper=0.02, gp_lengthscale=45, templates=templates)
+	demixer.train(epochs=epochs)
